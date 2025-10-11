0b760fe44cf4c0c6a5edbe00d78b190c5730d296
varshini <varshinic2004@gmail.com>
2025-10-11 19:48:51 +0530

    day4task

--- matrix.py
+++ matrix.py
@@ -1,16 +1,7 @@
+num = ("enter the number of matrix: ")
+print("enter the number of matrix :",num)
+total_sum = 0    
+for element in row:
+        total_sum += element
 
-matrix = []
-print("Enter 2x2 matrix elements:")
-for i in range(2):
-    row = []
-    for j in range(2):
-        num = int(input(f"Enter element [{i+1}][{j+1}]: "))
-        row.append(num)
-    matrix.append(row)
-total = 0
-for i in range(2):
-    for j in range(2):
-        total += matrix[i][j]
-
-print("Matrix is:", matrix)
-print("Sum of all elements:", total)
+print("Sum of all matrix elements:", total_sum)
